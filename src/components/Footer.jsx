31d78362888e5ba17e341545e9d32922bf571977
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2024-04-18 17:20:18 +0100

    up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
 <footer class=" rounded-lg shadow m-4 ">
     <div class="w-full mx-auto max-w-screen-xl p-4 flex justify-center items-center ">
-      <span class="text-sm text-gray-500 sm:text-center ">Powered by <a href="https://www.linkedin.com/in/othmane-tachafine-213275245/" target="_blank" class="text-white">Othmane Tachafine</a>
+      <span class="text-sm text-gray-500 sm:text-center ">Powered by <a href="https://linkedin.com/in/mouad-el-maslouhi-438038295" target="_blank" class="text-white">Mouad El Maslouhi </a> & <a href="https://www.linkedin.com/in/othmane-tachafine-213275245/" target="_blank" class="text-white">Otman Tachafine</a>
     </span>
     </div>
 </footer>
