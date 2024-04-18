5a05ad7fe7962c45e38f47c78f4cfb9e0fce29b3
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2024-04-18 22:44:06 +0100

    up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div class="w-full mx-auto max-w-screen-xl p-2 flex flex-col justify-center items-center ">
         <h1 class="text-sm text-gray-500 sm:text-center ">Powered by </h1>
         <div className="flex ">
-          <div className="flex flex-col md:flex-row justify-center items-center gap-1">
+          <div className="flex  justify-center items-center gap-1">
             <a
               href="https://linkedin.com/in/mouad-el-maslouhi-438038295"
               target="_blank"
@@ -27,9 +27,6 @@ const Footer = () => {
               class="text-white hover:text-blue-500 duration-150"
             >
               Othmane Tachafine
-            </a>
-            <a href="">
-              <FaGithub className=" cursor-pointer hover:fill-gray-700 duration-150" />
             </a>
           </div>
         </div>
