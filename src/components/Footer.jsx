66014f61a131bbf7d24268c942741d804356f709
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2024-04-18 21:49:44 +0100

    up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,38 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer class=" rounded-lg shadow m-4 ">
-      <div class="w-full mx-auto max-w-screen-xl p-4 flex flex-col justify-center items-center ">
+    <footer class=" rounded-lg shadow m-3 ">
+      <div class="w-full mx-auto max-w-screen-xl p-2 flex flex-col justify-center items-center ">
         <h1 class="text-sm text-gray-500 sm:text-center ">Powered by </h1>
-        <div> <a
-          href="https://linkedin.com/in/mouad-el-maslouhi-438038295"
-          target="_blank"
-          class="text-white"
-        >
-          Mouad El Maslouhi
-        </a> <span className="text-gray-500">&</span> <a
-          href="https://www.linkedin.com/in/othmane-tachafine-213275245/"
-          target="_blank"
-          class="text-white"
-        >
-          Othmane Tachafine
-        </a></div>
-       
+        <div className="flex ">
+          <div className="flex justify-center items-center gap-1">
+            <a
+              href="https://linkedin.com/in/mouad-el-maslouhi-438038295"
+              target="_blank"
+              class="text-white hover:text-blue-500 duration-150"
+            >
+              Mouad El Maslouhi
+            </a>
+            <a href="https://github.com/MOOUUAAD">
+              <FaGithub className=" cursor-pointer hover:fill-gray-700 duration-150" />
+            </a>
+          </div>
+          <span className="text-gray-500 mx-2"> & </span>{" "}
+          <div className="flex justify-center items-center gap-1">
+            <a
+              href="https://www.linkedin.com/in/othmane-tachafine-213275245/"
+              target="_blank"
+              class="text-white hover:text-blue-500 duration-150"
+            >
+              Othmane Tachafine
+            </a>
+            <a href="">
+              <FaGithub className=" cursor-pointer hover:fill-gray-700 duration-150" />
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
