8960f1340810e93e5f489dd46eaab597d89420c8
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2024-04-18 21:33:11 +0100

    up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    
+    <footer class=" rounded-lg shadow m-4 ">
+      <div class="w-full mx-auto max-w-screen-xl p-4 flex flex-col justify-center items-center ">
+        <h1 class="text-sm text-gray-500 sm:text-center ">Powered by </h1>
+        <div> <a
+          href="https://linkedin.com/in/mouad-el-maslouhi-438038295"
+          target="_blank"
+          class="text-white"
+        >
+          Mouad El Maslouhi
+        </a> <span className="text-gray-500">&</span> <a
+          href="https://www.linkedin.com/in/othmane-tachafine-213275245/"
+          target="_blank"
+          class="text-white"
+        >
+          Othmane Tachafine
+        </a></div>
+       
+      </div>
+    </footer>
+  );
+};
 
-<footer class=" rounded-lg shadow m-4 ">
-    <div class="w-full mx-auto max-w-screen-xl p-4 flex justify-center items-center ">
-      <span class="text-sm text-gray-500 sm:text-center ">Powered by <a href="https://linkedin.com/in/mouad-el-maslouhi-438038295" target="_blank" class="text-white">Mouad El Maslouhi </a> & <a href="https://www.linkedin.com/in/othmane-tachafine-213275245/" target="_blank" class="text-white">Otman Tachafine</a>
-    </span>
-    </div>
-</footer>
-
-  )
-}
-
-export default Footer
+export default Footer;
